b94ee788579d617a3fb75e887eec7bfd379a138c
haradhanc <deb_saheb8>
2018-05-17 17:23:15 +0530

    changes

--- SpecFlowProject/BatchFile.bat
+++
@@ -1,2 +0,0 @@
-packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe --out=TestResult.txt --result="TestResult.xml;format=nunit2" SpecFlowProject\bin\Debug\SpecFlowProject.dll
-packages\SpecFlow.2.3.2\tools\specflow.exe nunitexecutionreport SpecFlowProject\SpecFlowProject.csproj /out:MyResult.html
